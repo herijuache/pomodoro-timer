043ecda39befc96feb4010ca1671fe895dc20ca0
Heriberto <herijuache@gmail.com>
2022-07-04 19:29:10 -0700

    Updated timing mechanism. Need to work on reset button.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -6,30 +7,61 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✔"
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        lbl_timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        lbl_timer.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        lbl_timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    # catches when count goes to 0
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(math.floor(reps / 2)):
+            marks += "✔"
+        lbl_tracker.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-#
-count_down(5)
 
 # Image - Using Canvas widget
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -43,7 +75,7 @@ lbl_timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 lbl_timer.grid(row=0, column=1)
 
 # Button - Start
-btn_start = Button(text="Start", highlightthickness=0)
+btn_start = Button(text="Start", highlightthickness=0, command=start_timer)
 btn_start.grid(row=2, column=0)
 
 # Button - Reset
@@ -51,7 +83,7 @@ btn_reset = Button(text="Reset", highlightthickness=0)
 btn_reset.grid(row=2, column=2)
 
 # Label - Task
-lbl_tracker = Label(text=CHECKMARK, fg=GREEN, bg=YELLOW)
+lbl_tracker = Label(fg=GREEN, bg=YELLOW)
 lbl_tracker.grid(row=3, column=1)
 
 window.mainloop()
